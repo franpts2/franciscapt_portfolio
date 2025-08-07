d4e3c2f70384aabb4bf26589d80b258ad0dde029
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2025-08-07 08:15:03 +0100

    chore(app): Initial vite+react+tailwind configuration and also font setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+	return (
+		<>
+			<h1 className="text-2xl font-family-heading">Francisca Portugal</h1>
+			<p className="text-base font-family-body">Web developer!</p>
+		</>
+	);
+};
+
+export default App;
