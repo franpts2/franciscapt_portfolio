01d0749b891c1331fe63ee8e97fffbfc0aebebd2
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2025-08-20 20:12:40 +0100

    feat(portfolio): Added an icon to the site

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,17 @@ import ThemeSwitcher from "./ThemeSwitcher.js";
 const App = () => {
 	return (
 		<>
-            <ThemeSwitcher />
+			<div className="absolute top-4 right-4">
+				<ThemeSwitcher />
+			</div>
+
 			<div className="flex flex-col items-center justify-center min-h-screen gap-1">
 				<h1 className="text-2xl font-family-heading text-primary-accent">
 					Francisca Portugal
 				</h1>
-				<p className="text-base font-family-body text-primary">Web developer!</p>
+				<p className="text-base font-family-body text-primary">
+					Web developer!
+				</p>
 			</div>
 		</>
 	);
