8442786f164fdd88c2ea06a0f40fbc785d0faa97
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2025-08-20 19:07:07 +0100

    feat(home): Added bg and accent colors and centered the temporary content

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const App = () => {
 	return (
-		<>
-			<h1 className="text-2xl font-family-heading">Francisca Portugal</h1>
+		<div className="flex flex-col items-center justify-center min-h-screen gap-1">
+			<h1 className="text-2xl font-family-heading text-primary-accent">Francisca Portugal</h1>
 			<p className="text-base font-family-body">Web developer!</p>
-		</>
+		</div>
 	);
 };
 
