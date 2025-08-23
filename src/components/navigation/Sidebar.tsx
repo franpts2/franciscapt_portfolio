37f14c63304e8500d648092f9d683c10f8ee85d4
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2025-08-23 21:16:36 +0100

    feat(sidebar): Added visual feedback to show which page the user is on

--- src/components/navigation/Sidebar.tsx
+++ src/components/navigation/Sidebar.tsx
@@ -1,19 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
+	const [active, setActive] = useState("Home");
+
 	return (
 		<div className="flex flex-col text-center justify-center h-screen gap-4 ml-10">
-			<NavLink to="/" className="font-family-body text-lg">
-				Home
+			<NavLink
+				to="/"
+				className={({ isActive }) => {
+					return `font-family-body text-lg ${isActive ? "text-amber-600" : ""}`;
+				}}
+				onClick={() => setActive("home")}
+			>
+				<p>Home</p>
 			</NavLink>
-			<NavLink to="about" className="font-family-body text-lg">
+			<NavLink
+				to="about"
+				className={({ isActive }) => {
+					return `font-family-body text-lg ${isActive ? "text-amber-600" : ""}`;
+				}}
+				onClick={() => setActive("about")}
+			>
 				About
 			</NavLink>
-			<NavLink to="experience" className="font-family-body text-lg">
+			<NavLink
+				to="experience"
+				className={({ isActive }) => {
+					return `font-family-body text-lg ${isActive ? "text-amber-600" : ""}`;
+				}}
+				onClick={() => setActive("experience")}
+			>
 				Experience
 			</NavLink>
-			<NavLink to="projects" className="font-family-body text-lg">
+			<NavLink
+				to="projects"
+				className={({ isActive }) => {
+					return `font-family-body text-lg ${isActive ? "text-amber-600" : ""}`;
+				}}
+				onClick={() => setActive("projects")}
+			>
 				Projects
 			</NavLink>
 		</div>
