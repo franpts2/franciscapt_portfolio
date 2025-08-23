c64c0902297a32fee860aa4846f3fa8f21f755f0
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2025-08-23 21:06:22 +0100

    feat(portfolio): Added a fully functional sidebar, not completed style-wise

--- src/components/navigation/Sidebar.tsx
+++ src/components/navigation/Sidebar.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
-}
+	return (
+		<div className="flex flex-col text-center justify-center h-screen gap-4 ml-10">
+			<NavLink to="/" className="font-family-body text-lg">
+				Home
+			</NavLink>
+			<NavLink to="about" className="font-family-body text-lg">
+				About
+			</NavLink>
+			<NavLink to="experience" className="font-family-body text-lg">
+				Experience
+			</NavLink>
+			<NavLink to="projects" className="font-family-body text-lg">
+				Projects
+			</NavLink>
+		</div>
+	);
+};
 
-export default Sidebar
+export default Sidebar;
